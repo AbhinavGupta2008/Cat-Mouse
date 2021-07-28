af5a8789c2adcbd8add565ceec89208ec7203c94
AbhinavGupta2008 <83688265+AbhinavGupta2008@users.noreply.github.com>
2021-07-28 17:51:58 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,13 +4,13 @@ var cat, catImg1,catImg2;
 var mouse, mouseImg1,mouseImg2;
 
 function preload() {
-    bg = loadImage("images/garden.png");
-    catImg1= loadAnimation("images/catSitting.png");
-    catImg2=loadAnimation("images/catWalking1.png","images/catWalking2.png");
-    catImg3= loadAnimation("images/catHappy.png");
-    mouseImg1=loadAnimation("images/mouseGift.png");
-    mouseImg2= loadAnimation("images/mouseTease1.png","images/mouseTease2.png");
-    mouseImg3=loadAnimation("images/mouseHappy.png");
+    bg = loadImage("garden.png");
+    catImg1= loadAnimation("catSitting.png");
+    catImg2=loadAnimation("catWalking1.png","catWalking2.png");
+    catImg3= loadAnimation("catHappy.png");
+    mouseImg1=loadAnimation("mouseGift.png");
+    mouseImg2= loadAnimation("mouseTease1.png","mouseTease2.png");
+    mouseImg3=loadAnimation("mouseHappy.png");
 }
 
 function setup(){
@@ -23,7 +23,6 @@ function setup(){
     cat = createSprite(900, 600);
     cat.addAnimation("catSleeping", catImg1);
     cat.scale = 0.25;
-    cat.debug=true
 
     makeCatWalk();
 
